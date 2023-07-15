696f770f4dbb171ddc866b3a2db5610737d70f33
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-15 06:28:17 -0500

    created a reuasable modal component

---
+++ components/ui/modal.tsx
@@ -0,0 +1,47 @@
+"use client";
+// REUSABLE MODAL COMPONENT
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+interface ModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+export const Modal: React.FC<ModalProps> = ({
+  // extracting our props
+  title,
+  description,
+  isOpen,
+  onClose,
+  children,
+}) => {
+  // onChange will accept the param of "open" which can be either true or false
+  // If !open, this function will trigger onClose()
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
