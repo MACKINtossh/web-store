0fcbef13ef0c8fcaf231abf52028d8dae9c065e2
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-15 06:32:04 -0500

    created the main setup page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <p>Clean Up</p>;
+"use client";
+// GLOBAL IMPORTS
+import { useEffect } from "react";
+import { UserButton } from "@clerk/nextjs";
+
+// LOCAL IMPORTS
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
 }
